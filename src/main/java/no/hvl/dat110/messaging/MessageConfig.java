cba10e3cfbd357724a57c236f3923ddd8b93e19f
Lars Michael Kristensen <lmkr@users.noreply.github.com>
2023-01-22 16:52:51 +0100

    deleted MessageConfig class which was not used

--- src/main/java/no/hvl/dat110/messaging/MessageConfig.java
+++
@@ -1,9 +0,0 @@
-package no.hvl.dat110.messaging;
-
-public class MessageConfig {
-
-	public static final int SEGMENTSIZE = 128;
-	
-	public static final int MESSAGINGPORT = 8080;
-	public static final String MESSAGINGHOST = "localhost";
-}
